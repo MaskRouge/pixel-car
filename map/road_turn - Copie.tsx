8a48581bad4d5c8f53b6731cd7d261ef2cbe76b2
MaskRouge <cyril@smallcar.be>
2025-09-07 20:07:38 +0200

    07-09-25

---
+++ map/road_turn - Copie.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="road_turn - Copie" tilewidth="64" tileheight="64" tilecount="4" columns="2">
+ <image source="jeu_de_tuile/road_turn - Copie.png" width="128" height="128"/>
+</tileset>
---
+++ map/road_white_r.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="road_white_r" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+ <image source="jeu_de_tuile/road_white_r.png" width="64" height="64"/>
+</tileset>
